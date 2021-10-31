1f34c7252294a02c8384545b0647a81b140e44e4
John Wilken Christopher <92281847+jhnwlkn@users.noreply.github.com>
2021-10-31 16:07:40 +0700

    Add files via upload

---
+++ Material/Opration, List, n Condition.py
@@ -0,0 +1,53 @@
+'''
+#Input 
+nama = input("Masukan nama Anda : ")
+print ("Nama = " + nama)
+'''
+
+'''
+#String Operations
+a = "Hello world"
+print (a[1]) #print char ke ..
+
+print (a[2:7]) #dari berapa ke berapa ='char 2 ke char 7'
+
+print (len(a)) #mencari panjang string a = 11
+
+print (x[-1]) #mencetak dari belakang = 'd'
+
+a = "Hello"
+b = "world"
+c = a + " " + b
+print (c)
+
+# String Formating
+age = 18
+nama = "John"
+txt = "Nama saya adalah {}, dan umur saya {} tahun".format(nama, age) #berguna untuk memasukan nilai variabel
+txt = f"Nama saya adalah {nama}, dan umur saya {age} tahun" #formating yang lebih simpel
+print (txt)
+
+txt = input("Masukan nama ")
+print ("nama : {txt}")
+'''
+
+'''
+# Booleans
+a = input("Masukan nilai a : ")
+b = input("Masukan niali b : ")
+if b > a : #condition
+    print ("b lebih besar dari a")
+else:
+    print ("b lebih kecil dari a")
+'''
+
+'''
+# Python List
+
+mylist = []
+mylist.append(1) #memasukan nilai kedalam list
+mylist.append(2)
+mylist.append(3)
+print(mylist) # mencetak semua isi list mylist
+print(mylist[0]) # mencetak data ke ..
+'''
